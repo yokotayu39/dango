8f3de3a268a4dc866cd4b41eb40f431a89810363
jamashita <jamashita@arachnid.xyz>
2023-09-18 18:34:18 +0900

    add a subject

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
-import { NextPage } from 'next';
+import {promises} from 'fs';
+import { GetStaticProps, NextPage } from 'next';
+import * as path from 'path';
+import * as process from 'process';
 import React, { ReactElement, useEffect, useState } from 'react';
 import { Button } from '../components/Button';
+
+type Country = {
+  jpnName: string;
+  engName: string;
+  numeric: number;
+  alpha3: string;
+  alpha2: string;
+  location: string;
+  subDivision: string;
+};
 
 type RandomCat = {
   id: string;
@@ -9,7 +22,12 @@ type RandomCat = {
   height: number;
 };
 
-const IndexPage: NextPage = (): ReactElement => {
+type Props = {
+  countries: Array<Country>;
+};
+
+
+const IndexPage: NextPage<Props> = ({ countries }: Props): ReactElement => {
   const [count, setCount] = useState<number>(0);
   const [labourHours, setLabourHours] = useState<string>('0');
   const [catImage, setCatImage] = useState<null | RandomCat>(null);
@@ -87,14 +105,34 @@ const IndexPage: NextPage = (): ReactElement => {
         </div>
       </div>
       <div className="m-10 p-4 w-2/3 mx-auto shadow-lg border-2 rounded-2xl">
+        <ul className="list-none">
+        {countries.map((country: Country) => {
+          return (
+            <li className="text-gray-800 even:bg-teal-100 text-lg">
+              <div className="my-1">{country.jpnName}</div>
+              <div className="my-1">{country.engName}</div>
+            </li>
+          );
+        })}
+        </ul>
+      </div>
+      <div className="m-10 p-4 w-2/3 mx-auto shadow-lg border-2 rounded-2xl">
         {catImage === null ? null: <img src={catImage.url} height={catImage.height} width={catImage.width} alt="cat" className="w-full" />}
       </div>
     </>
   );
 };
 
-// export const getStaticProps: GetStaticProps = () => {
-// };
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const buffer = await promises.readFile(path.join(process.cwd(), 'json', 'countries.json'));
+  const str  = buffer.toString();
+
+  return {
+    props: {
+      countries: JSON.parse(str)
+    }
+  };
+};
 
 // eslint-disable-next-line import/no-default-export
 export default IndexPage;
